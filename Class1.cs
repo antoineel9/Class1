5af18c4b41ea5a766ed0148317a0428672c4f8b7
Antoine Williams <54339074+antoineel9@users.noreply.github.com>
2019-10-03 12:11:09 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Cinema2.Models
+{
+    public class Movies
+    {
+        public string MovieId { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Rating { get; set; }
+    }
+}
